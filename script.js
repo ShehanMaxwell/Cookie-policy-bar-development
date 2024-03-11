10db13533ff2cf54bfc95886786e68cb8552a49b
Shehan Maxwell <shehanbbn@gmail.com>
2024-03-11 22:05:25 +0530

    added 3 files to .

---
+++ script.js
@@ -0,0 +1,12 @@
+document
+  .getElementById('cookie-preference-btn')
+  .addEventListener('click', function () {
+    document.getElementById('cookie-preference-overlay').style.display =
+      'block';
+  });
+
+// document
+//   .getElementById('save-cookie-preference')
+//   .addEventListener('click', function () {
+//     document.getElementById('cookie-preference-overlay').style.display = 'none';
+//   });
